fbfd71a129e1e4a1bb1dc2472babc1e244d265c5
Josh Hawkins <jkhaccounts@gmail.com>
2017-04-10 23:13:09 -0500

    Hotfix item preload

--- src/states/preload.js
+++ src/states/preload.js
@@ -6,6 +6,10 @@ class Preload extends Phaser.State {
     this.game.load.spritesheet('player', 'assets/images/chara2.png', 26, 36);
     this.game.load.image('bullet', 'assets/images/bullet.png');
 
+    // Load items
+    this.game.load.image('Pickaxe', 'assets/images/pickaxe.png');
+    this.game.load.image('boulder', 'assets/images/boulder.png');
+
     // Load boss
     this.game.load.spritesheet('boss', 'assets/images/elemental.png', 120, 129);
     this.game.load.spritesheet('boss1bullet', 'assets/images/waterbullet.png', 40, 56);
