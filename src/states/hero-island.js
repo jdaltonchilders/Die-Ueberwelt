ac84e5cc681a06ffd94ae7c42a54a3e7865c4c77
Josh Hawkins <jkhaccounts@gmail.com>
2017-04-30 17:49:43 -0500

    Add temporary bullet fix

--- src/states/hero-island.js
+++ src/states/hero-island.js
@@ -303,12 +303,27 @@ export default class HeroIsland extends Phaser.State {
     // Update NPCs
     this.npcs.forEach(npc => npc.update());
 
+    // Collide player bullets with barriers;
+    const barriers = [
+      this.treesTrunk,
+      this.houses,
+      this.housesRoof,
+      this.doors,
+      this.trees
+    ];
+    // TODO: Collide with the `barriers` layers instead of just collision layer
+    // barriers.forEach(layer =>
+    this.game.physics.arcade.collide(
+      this.collisionLayer, // layer
+      this.player.bullets,
+      (bullet, layer) => {
+        bullet.kill();
+      }
+    );
+    // ));
+
     // Collide with Layers
     this.game.physics.arcade.collide(this.player.sprite, this.collisionLayer);
-
-    /**
-         * Use this.player.sprite.world.x/y to get position
-         */
 
     // Check if Enter Hero House contains the Player
     if (
