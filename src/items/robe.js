903e57b87cbc45be3b1450233597cb0ae3628ff2
Josh Hawkins <jkhaccounts@gmail.com>
2017-05-01 12:23:01 -0500

    Add dialogue to new items

--- src/items/robe.js
+++ src/items/robe.js
@@ -1,8 +1,21 @@
 import Item from '../gui/item';
+import Dialogue from '../utilities/dialogue';
 import store from '../store';
 
 export default (game, x, y, player) => {
-  var item = new Item(game, x, y, 'Robe', 'Robe', player);
+  var item = new Item(
+    game,
+    x,
+    y,
+    'Robe',
+    'Robe',
+    player,
+    new Dialogue([
+      "You stole the neighbor's robe. Jerk...",
+      'I hope your increased armor soothes your conscience.',
+      ''
+    ])
+  );
   item.sprite.scale.set(0.75, 0.75);
   // item.sprite.smoothed = false;
   item.afterPickup = () => {
--- src/states/preload.js
+++ src/states/preload.js
@@ -13,7 +13,7 @@ class Preload extends Phaser.State {
     // Load Player
     this.game.load.spritesheet('player', 'assets/images/chara2.png', 26, 37);
     this.game.load.image('bullet', 'assets/images/bullet.png');
-    
+
     // Load NPCs
     this.game.load.spritesheet('npc', 'assets/images/npc3.png', 26, 37);
 
@@ -23,12 +23,11 @@ class Preload extends Phaser.State {
     this.game.load.image('Shoes', 'assets/images/shoes.png');
     this.game.load.image('Robe', 'assets/images/robe.png');
     this.game.load.image('boulder', 'assets/images/boulder.png');
-    
+
     // Load monsters
     this.game.load.spritesheet('wolf', 'assets/images/monster_wolf1.png', 64, 66);
     this.game.load.spritesheet('treant', 'assets/images/monster_golem2.png', 47, 50);
-    
-    
+
     // Load boss
     this.game.load.spritesheet('boss', 'assets/images/elemental.png', 120, 129);
     this.game.load.spritesheet('boss1bullet', 'assets/images/waterbullet.png', 40, 56);
